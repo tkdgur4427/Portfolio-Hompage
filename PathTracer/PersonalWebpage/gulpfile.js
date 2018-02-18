ee152952fd301b6847830cf94ccc00a2c2cb1085
Brian Hong <tkdgur4427@outlook.com>
2018-02-18 22:03:09 +0900

    commit

--- PathTracer/PersonalWebpage/gulpfile.js
+++ PathTracer/PersonalWebpage/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='copy' />
+﻿/// <binding AfterBuild='copy, clean' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
@@ -7,20 +7,24 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 var gulp = require('gulp');
 var gulp_minify = require('gulp-clean-css');
 var gulp_rename = require('gulp-rename');
+var gulp_watch = require('gulp-watch');
 
 gulp.task('copy', function () {
     // copy pure-min.css file to the wwwroot/css directory
-    gulp.src("./node_modules/purecss/build/pure-min.css").pipe(gulp.dest("./wwwroot/css"));
+    //gulp.src("./node_modules/purecss/build/pure-min.css").pipe(gulp.dest("./wwwroot/css"));
 });
 
 gulp.task('clean', function () {
-    gulp.src("./dev/css/global.css")
-        .pipe(gulp_rename(function (path) {
-            path.basename += "-min";
-        }))
-        .pipe(gulp_minify({ debug: true }, (details) => {
-            console.log(details.name + ":" + details.stats.originalSize);
-            console.log(details.name + ":" + details.stats.minifiedSize);
-        }))       
-        .pipe(gulp.dest("./wwwroot/css"));
+    // watch any changes exists, if it is run the clean
+    gulp_watch('./dev/css/global.css', function () {
+        gulp.src("./dev/css/global.css")
+            .pipe(gulp_rename(function (path) {
+                path.basename += "-min";
+            }))
+            .pipe(gulp_minify({ debug: true }, (details) => {
+                console.log(details.name + ":" + details.stats.originalSize);
+                console.log(details.name + ":" + details.stats.minifiedSize);
+            }))
+            .pipe(gulp.dest("./wwwroot/css"));
+    });
 });
